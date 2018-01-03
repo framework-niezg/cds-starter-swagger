d82a7a0f2758c26573299ae622f32ef61df0e5c8
niezg <niezg@zjcds.com>
2018-01-03 15:59:36 +0800

    初始化提交

---
+++ src/main/java/com/zjcds/common/swagger/Swagger2Configuration.java
@@ -0,0 +1,52 @@
+package com.zjcds.common.swagger;
+
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * created date：2017-08-17
+ *
+ * @author niezhegang
+ */
+@Configuration
+@EnableSwagger2
+@Profile("dev")
+public class Swagger2Configuration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public Docket docket(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                //.apis(RequestHandlerSelectors.basePackage("com.zjcds.dp.cdm.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/doc", "/api/swagger-ui.html");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+        registry.addRedirectViewController("/api/swagger-resources/**", "/swagger-resources/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.
+                addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.
+                addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
